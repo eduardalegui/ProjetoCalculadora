5d328f339744078da9ce4e3f7a9356972bec8f31
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-03-14 15:45:45 -0300

    save 3

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function insert (num){
-    var texto = document.getElementById('resultado').innerHTML;
+    var texto = document.getElementById('calculo').innerHTML;
     if(texto=== '0'){
-        document.getElementById('resultado').innerHTML = num;
+        document.getElementById('calculo').innerHTML = num;
     }else{
-        document.getElementById('resultado').innerHTML = texto + num;
+        document.getElementById('calculo').innerHTML = texto + num;
     }
+}
+function clean(){
+    document.getElementById('calculo').innerHTML= '0';
+    document.getElementById('resposta').innerHTML= '0';
+}
+function back(){
+    var texto = document.getElementById('calculo').innerHTML;
+    document.getElementById('calculo').innerHTML = texto.substring(0,texto.length -1);
+}
+function calcular(){
+    var texto = document.getElementById('calculo').innerHTML;
+    var texto = eval(texto);
+    document.getElementById('resposta').innerHTML= texto;
+    document.getElementById('calculo').innerHTML= '0';
 }
